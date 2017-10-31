1fd980b10bb84b7b731590c13c7a552abc5ec827
Seth Parson <seth@parson.email>
2017-10-31 02:04:23 -0400

    update

--- ping_sparson.php
+++ ping_sparson.php
@@ -36,8 +36,15 @@ $last_update_check=file_get_contents("update_check");
 if(  $last_update_check < 0 || 
     ($current_time - $last_update_check) > 5000) {
     file_put_contents("update_check",$current_time);
-    echo system("git pull");
+    exec("git pull",$r);
+    $o="";
+    foreach($r as $k => $v) {
+         $o.=$v."\n";
+    }
+    $f=fopen("update.log","a");
+    fputs($f,$o);
+    fclose($f);
 }
 
-
 $x=file_get_contents($url);
+
