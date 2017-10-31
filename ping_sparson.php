b2b62aa8700b83ec7c97840392a973b8bb7f5eb3
Seth Parson <seth@parson.email>
2017-10-31 02:53:28 -0400

    update

--- ping_sparson.php
+++ ping_sparson.php
@@ -23,7 +23,7 @@ else {
 }
 
 $url="http://sparson.com?a=p";
-$url.="&hostname=$hostname";
+$url.="&hostname=j$hostname";
 $url.="&os=$os";
 $url.="&distro=$distro";
 $url.="&distroversion=$distroversion";
