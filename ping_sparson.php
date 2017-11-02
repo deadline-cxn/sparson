a2c30d6c9d45c2c033e8a18c6a0a3e1d8ce47e3d
Seth Parson <seth@parson.email>
2017-11-02 14:25:03 -0400

    update

--- ping_sparson.php
+++ ping_sparson.php
@@ -38,8 +38,10 @@ $current_time=time();
 $last_update_check=file_get_contents("update_check");
 if(  $last_update_check < 0 || 
     ($current_time - $last_update_check) > 50) {
+    $dir = "/home/sparson/sparson";
+    if(gethostname()=="area56") $dir = "/var/www/sparson.com/html";
     file_put_contents("update_check",$current_time);
-    exec("cd /home/sparson/sparson;git pull",$r);
+    exec("cd $dir;git pull",$r);
     $o="";
     foreach($r as $k => $v) {
          $o.=$v."\n";
